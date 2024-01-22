3d6918b5ce31f8602ffedc37200523be2ac3f671
ron <rinbar70@gmail.com>
2024-01-22 12:43:28 +0200

    Replace 'react' with './my/react'.

--- src/Section.js
+++ src/Section.js
@@ -1,13 +1,13 @@
-import { useContext } from 'react';
+import { useContext } from './my/react';
 import { LevelContext } from './LevelContext.js';
 
 export default function Section({ children }) {
-  const level = useContext(LevelContext);
-  return (
-    <section className="section">
-      <LevelContext.Provider value={level + 1}>
-        {children}
-      </LevelContext.Provider>
-    </section>
-  );
+    const level = useContext(LevelContext);
+    return (
+        <section className="section">
+            <LevelContext.Provider value={level + 1}>
+                {children}
+            </LevelContext.Provider>
+        </section>
+    );
 }
