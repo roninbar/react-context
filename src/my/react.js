51364b9413ade07107db619652da7fd0dc1fd417
ron <rinbar70@gmail.com>
2024-01-22 12:44:10 +0200

    Extract `Context` class.

--- src/my/react.js
+++ src/my/react.js
@@ -1,40 +1,48 @@
 import { ReactElement } from 'react';
 
-/**
- * @param {any} defaultValue Value returned by `useContext` outside a `<Provider>`.
- * @returns {{Provider: ({value: any, children: ReactElement[]}) => ReactElement}} An context object that contains a `<Provider>` component.
- */
-export function createContext(defaultValue) {
-    let currentValue = defaultValue;
+class Context {
+    constructor(defaultValue) {
+        let currentValue = defaultValue;
 
-    /**
-     * @param {{value: any, children: ReactElement[]}} props
-     */
-    function Provider({ value, children }) {
-        return (<>
-            {children.map((child, index) => {
-                function Child(props) {
-                    currentValue = value;
-                    try {
-                        return child.type(props);
-                    } finally {
-                        currentValue = defaultValue;
+        /**
+         * @param {{value: any, children: ReactElement[]}} props
+         */
+        this.Provider = function ({ value, children }) {
+            return <>
+                {children.map((child, index) => {
+                    function Child(props) {
+                        currentValue = value;
+                        try {
+                            return child.type(props);
+                        } finally {
+                            currentValue = defaultValue;
+                        }
                     }
-                }
-                return (
-                    (<Child {...child.props} key={child.key === null ? index : child.key}>
-                        {child.props.children}
-                    </Child>)
-                );
-            })}
-        </>);
-    }
+                    return (
+                        (<Child {...child.props} key={child.key === null ? index : child.key}>
+                            {child.props.children}
+                        </Child>)
+                    );
+                })}
+            </>;
+        }
 
-    return { Provider, _use: () => currentValue };
+        this._use = function () {
+            return currentValue;
+        };
+    }
 }
 
 /**
- * @param {{Provider: ({value: any, children: ReactElement[]}) => ReactElement}} context A function returned by `createContext`.
+ * @param {any} defaultValue Value returned by `useContext` outside a `<Provider>`.
+ * @returns {Context} An context object that contains a `<Provider>` component.
+ */
+export function createContext(defaultValue) {
+    return new Context(defaultValue);
+}
+
+/**
+ * @param {Context} context An context object returned by `createContext`.
  */
 export function useContext(context) {
     return context._use();
